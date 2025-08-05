c7c279639a9bd1d5d0bd010aed8def7f64a60345
hppro2024\smcore2024 <klpark@smcore.co.kr>
2025-08-05 16:29:36 +0900

    feat(mvc): 웹소켓 동시 접속 수 지표 추가

--- chat-mvc/src/main/java/com/example/chat_mvc/listener/WebSocketEventListener.java
+++ chat-mvc/src/main/java/com/example/chat_mvc/listener/WebSocketEventListener.java
@@ -3,10 +3,16 @@ package com.example.chat_mvc.listener;
 import com.example.chat_mvc.common.RoomUserSessionManager;
 import com.example.chat_mvc.dto.WebSocketRoomUser;
 import com.example.chat_mvc.service.ChatRoomService;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @RequiredArgsConstructor
@@ -14,6 +20,15 @@ public class WebSocketEventListener {
 
     private final RoomUserSessionManager roomUserSessionManager;
     private final ChatRoomService chatRoomService;
+    private final AtomicInteger activeConnections = new AtomicInteger(0);
+
+    /**
+     * 웹소켓 Connect 이벤트가 발생했을 때의 로직
+     */
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectEvent event) {
+        activeConnections.incrementAndGet();
+    }
 
     /**
      * 웹소켓 Disconnect 이벤트가 발생했을 때의 로직
@@ -26,6 +41,12 @@ public class WebSocketEventListener {
         if (roomUser != null) {
             // 해당 사용자 퇴장 처리
             chatRoomService.exitRoom(roomUser.getRoomId(), roomUser.getUserId());
+            activeConnections.decrementAndGet();
         }
+    }
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizer() {
+        return meterRegistry -> meterRegistry.gauge("chat_app_active_connections", activeConnections);
     }
 }
