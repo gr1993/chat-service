9849e8603209805ca41ea6fdc34b6cd886f1b4fe
hppro2024\smcore2024 <klpark@smcore.co.kr>
2025-08-19 10:46:35 +0900

    feat(webflux): cors 설정 추가

---
+++ chat-webflux/src/main/java/com/example/chat_webflux/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.chat_webflux.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
